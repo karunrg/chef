318286331651c37afb4b6bc3f57366d13f6cde91
Reddy <karunrg@gmail.com>
2016-02-24 00:35:20 -0500

    update

--- cookbooks/sre_tc_1a/metadata.rb
+++ cookbooks/sre_tc_1a/metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'karunrg@gmail.com'
 license 'all_rights'
 description 'Installs/Configures sre_tc_1a'
 long_description 'Installs/Configures sre_tc_1a'
-version '0.1.1'
+version '0.2.0'
 
 depends 'nginx', '~> 2.7.6'
--- cookbooks/sre_tc_1a/recipes/web.rb
+++ cookbooks/sre_tc_1a/recipes/web.rb
@@ -6,22 +6,55 @@
 
 
 # Create the document log directory.
-directory node['sre_tc_1a']['git']['log_dir'] do
-  recursive true
-end
 
 include_recipe 'nginx::default'
 
-template "#{node['nginx']['dir']}/sites-available/git.conf" do
+data_bag("vhosts").each do |site|
+  site_data = data_bag_item("vhosts",site)
+  site_name = site_data["id"]
+
+directory "#{node['nginx']['log_dir']}/#{site_name}" do
+  mode      node['nginx']['log_dir_perm']
+  owner     node['nginx']['user']
+  action    :create
+  recursive true
+end
+
+directory "#{node['nginx']['default_root']}/#{site_name}" do
+  mode      node['nginx']['log_dir_perm']
+  owner     node['nginx']['user']
+  action    :create
+  recursive true
+end
+
+template "#{node['nginx']['dir']}/sites-available/#{site_name}.conf" do
   source 'custom-site.erb'
   owner  'root'
   group  node['root_group']
   mode   '0644'
+  variables(
+  :site_name => site_data["id"],
+  :port => site_data["port"]
+  )
   notifies :reload, 'service[nginx]', :delayed
 end
 
+template "#{node['nginx']['default_root']}/#{site_name}/index.html" do
+  source 'index-site.erb'
+  mode      node['nginx']['log_dir_perm']
+  owner     node['nginx']['user']
+  action    :create
 
-nginx_site 'git.conf' do
+  variables(
+  :site_name => site_data["id"],
+  :port => site_data["port"]
+  )
+end
+
+
+nginx_site "#{site_name}.conf" do
   enable node['nginx']['default_site_enabled']
+ end
+
 end
 
